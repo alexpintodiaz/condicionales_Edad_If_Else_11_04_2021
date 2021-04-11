de193c568b1bba572ababf192f81bec25d657497
alexpintodiaz <alexanderp143@hotmail.com>
2021-04-11 12:42:56 -0500

    11_04_2021_taller_2.1

---
+++ main.js
@@ -0,0 +1,6 @@
+let edad = parseInt(prompt("Ingrese por favor su edad"));
+if(edad >= 18){
+    console.log("Es mayor de edad");
+}else{
+    console.log("La edad ingresada no cumple con los parámetros");
+}
